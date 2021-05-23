51c32280b458724cc37f355650ce880fcb63de95
Anton Skorup <anton@skorup.se>
2021-05-23 21:23:26 +0200

    Initial commit

---
+++ src/se/skorup/main/Main.java
@@ -0,0 +1,16 @@
+package se.skorup.main;
+
+/**
+ * The class responsible for starting the program,
+ * i.e. the main class.
+ * */
+public class Main
+{
+    /**
+     * The main method starts the program.
+     * */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
