34b57e425b3e448ba09fa0ef0ad0d157b30fa2a2
Anton Skorup <antonskorup@gmail.com>
2021-05-24 20:00:27 +0200

    More work on the GUI

--- src/se/skorup/main/gui/frames/MainFrame.java
+++ src/se/skorup/main/gui/frames/MainFrame.java
@@ -2,9 +2,13 @@ package se.skorup.main.gui.frames;
 
 import se.skorup.API.DebugMethods;
 import se.skorup.API.Utils;
+import se.skorup.main.gui.panels.ControlPanel;
 import se.skorup.main.manager.GroupManager;
+import se.skorup.main.objects.Person;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -16,14 +20,38 @@ import java.util.List;
  * */
 public class MainFrame extends JFrame
 {
+    /** The group managers. */
+    private final List<GroupManager> managers = new ArrayList<>();
+
+    /** Adds a demo group if {@code true}. */
+    private final boolean debug = true;
+
     /** The frame's container. */
     private final Container cp = this.getContentPane();
 
-    /** The layout of the frame.*/
+    /** The layout of the frame. */
     private final BorderLayout layout = new BorderLayout();
 
-    /** The group managers. */
-    private final List<GroupManager> managers = new ArrayList<>();
+    /** The layout of the container. */
+    private final BorderLayout pContainerLayout = new BorderLayout();
+
+    /** The control panel of this frame. */
+    private ControlPanel ctrPanel;
+
+    /** The container panel. */
+    private final JPanel pContainer = new JPanel();
+
+    /** Spacer */
+    private final JLabel lblSpacer1 = new JLabel(" ");
+
+    /** Spacer */
+    private final JLabel lblSpacer2 = new JLabel("   ");
+
+    /** Spacer */
+    private final JLabel lblSpacer3 = new JLabel("   ");
+
+    /** Spacer */
+    private final JLabel lblSpacer4 = new JLabel("   ");
 
     /**
      * Creates a new MainFrame.
@@ -31,7 +59,23 @@ public class MainFrame extends JFrame
     public MainFrame()
     {
         super("Gruppskapare");
+        this.demoGroup();
         this.init();
+    }
+
+    /**
+     * Adds a demo group if debug param is {@code true}.
+     * */
+    private void demoGroup()
+    {
+        if (debug)
+        {
+            var gm = new GroupManager("DEMO - Grupp");
+            gm.registerPerson("Anton", Person.Role.LEADER);
+            gm.registerPerson("Sebbe", Person.Role.CANDIDATE);
+
+            managers.add(gm);
+        }
     }
 
     /**
@@ -42,8 +86,24 @@ public class MainFrame extends JFrame
         DebugMethods.log("Starting initialization of MainFrame.", DebugMethods.LogType.DEBUG);
         this.setProperties();
         DebugMethods.log("The properties has been set.", DebugMethods.LogType.DEBUG);
+        this.addComponents();
+        DebugMethods.log("The components has been added.", DebugMethods.LogType.DEBUG);
         this.setVisible(true);
         DebugMethods.log("Frame is now initialized", DebugMethods.LogType.DEBUG);
+    }
+
+    /**
+     * Adds the components.
+     * */
+    private void addComponents()
+    {
+        pContainer.add(ctrPanel, BorderLayout.PAGE_START);
+
+        cp.add(lblSpacer1, BorderLayout.PAGE_START);
+        cp.add(lblSpacer2, BorderLayout.LINE_START);
+        cp.add(ctrPanel, BorderLayout.CENTER);
+        cp.add(lblSpacer3, BorderLayout.LINE_END);
+        cp.add(lblSpacer4, BorderLayout.PAGE_END);
     }
 
     /**
@@ -57,5 +117,10 @@ public class MainFrame extends JFrame
 
         cp.setBackground(Utils.BACKGROUND_COLOR);
         cp.setLayout(layout);
+
+        ctrPanel = new ControlPanel(this, managers);
+
+        pContainer.setBackground(Utils.BACKGROUND_COLOR);
+        pContainer.setLayout(pContainerLayout);
     }
 }
--- src/se/skorup/main/gui/panels/ControlPanel.java
+++ src/se/skorup/main/gui/panels/ControlPanel.java
@@ -1,9 +1,12 @@
 package se.skorup.main.gui.panels;
 
+import se.skorup.API.Utils;
 import se.skorup.main.gui.frames.MainFrame;
 import se.skorup.main.manager.GroupManager;
 
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import java.awt.FlowLayout;
 import java.util.List;
 
 /**
@@ -14,7 +17,14 @@ public class ControlPanel extends JPanel
     /** The reference to the managers. */
     private final List<GroupManager> managers;
 
+    /** The instance of the MainFrame in use. */
     private final MainFrame mf;
+
+    /** The combo box holding the group managers. */
+    private final JComboBox<GroupManager> cbManagers = new JComboBox<>();
+
+    /** The layout of the panel. */
+    private final FlowLayout layout = new FlowLayout(FlowLayout.LEFT);
 
     /**
      * Creates a new ControlPanel
@@ -26,5 +36,43 @@ public class ControlPanel extends JPanel
     {
         this.mf = mf;
         this.managers = managers;
+
+        this.setProperties();
+        this.addComponents();
+    }
+
+    private void updateManagers()
+    {
+        // Clears the list.
+        cbManagers.removeAllItems();
+
+        // Adds the items.
+        managers.forEach(cbManagers::addItem);
+
+        // Adds dummy item used to detect clicks.
+        cbManagers.addItem(new GroupManager("Lägg till en grupp"));
+    }
+
+    /**
+     * Sets the properties.
+     * */
+    private void setProperties()
+    {
+        this.setBackground(Utils.BACKGROUND_COLOR);
+        this.setForeground(Utils.FOREGROUND_COLOR);
+        this.setLayout(layout);
+
+        cbManagers.setBackground(Utils.COMPONENT_BACKGROUND_COLOR);
+        cbManagers.setForeground(Utils.FOREGROUND_COLOR);
+
+        this.updateManagers();
+    }
+
+    /**
+     * Adds the components.
+     * */
+    private void addComponents()
+    {
+        this.add(cbManagers);
     }
 }
