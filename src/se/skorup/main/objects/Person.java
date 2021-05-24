bca47fff941ea0a293286da906ac59ece34c9e63
Anton Skorup <antonskorup@gmail.com>
2021-05-24 10:56:08 +0200

    Implemented base person class

---
+++ src/se/skorup/main/objects/Person.java
@@ -0,0 +1,49 @@
+package se.skorup.main.objects;
+
+/**
+ * The supertype for all persons.
+ * */
+public abstract class Person
+{
+    /** The name of the person. */
+    private final String name;
+
+    /** The id of the person. */
+    private final int id;
+
+    /**
+     * Creates a new person with a name.
+     *
+     * @param name the name of the person.
+     * @param id the id of the person.
+     * */
+    public Person(String name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "%s: %d".formatted(name, id);
+    }
+
+    /**
+     * A person is equal to another person
+     * iff their id's are the same.
+     *
+     * @param o the other person to compare against.
+     * */
+    @Override
+    public boolean equals(Object o)
+    {
+        return o instanceof Person p && id == p.id;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return id;
+    }
+}
