e4c4c121a0247d0f17c764e126f059c12a121ab3
Anton Skorup <anton@skorup.se>
2021-11-09 15:34:03 +0100

    Version bump

--- src/se/skorup/API/util/Utils.java
+++ src/se/skorup/API/util/Utils.java
@@ -45,7 +45,7 @@ public class Utils
 
     // About
     /** The version of the program. */
-    public static final String VERSION = "0.3.0.2";
+    public static final String VERSION = "0.3.0.3";
 
     /** The about string. */
     public static final String ABOUT =
