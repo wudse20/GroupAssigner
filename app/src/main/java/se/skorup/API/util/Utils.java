42d3e630f7e5dbd7525da0d83e096cbef11c638d
Anton Skorup <anton@skorup.se>
2022-09-09 14:27:19 +0200

    Relase

--- app/src/main/java/se/skorup/API/util/Utils.java
+++ app/src/main/java/se/skorup/API/util/Utils.java
@@ -60,7 +60,7 @@ public class Utils
 
     // About
     /** The version of the program. */
-    public static final String VERSION = "0.4.3.1-Indev";
+    public static final String VERSION = "0.4.3.1";
 
     /** The about string. */
     public static final String ABOUT =
