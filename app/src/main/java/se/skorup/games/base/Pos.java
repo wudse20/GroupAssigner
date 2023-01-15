42ce487573f947cfe8b88c7af26ba3b0bde9211e
Anton Skorup <anton@skorup.se>
2023-01-15 11:24:26 +0100

    Some testing.

--- app/src/main/java/se/skorup/games/base/Pos.java
+++ app/src/main/java/se/skorup/games/base/Pos.java
@@ -34,19 +34,4 @@ public record Pos(int x, int y)
     {
         return new Pos(x * i, y * i);
     }
-
-    /**
-     * Carries out an element by element addition of the points.
-     * <br><br>
-     * {@code var p1 = new Pos(a, b);}<br>
-     * {@code var p2 = new Pos(c, d);}<br>
-     * {@code p1.add(p2) = new Pos(a + c, b + d)}
-     *
-     * @param p the position to add with.
-     * @return the result of the addition as a new instance.
-     * */
-    public Pos add(Pos p)
-    {
-        return new Pos(x + p.x, y + p.y);
-    }
 }
