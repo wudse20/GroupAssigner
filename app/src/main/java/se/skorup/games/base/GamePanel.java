42b38cd82b4a25ceec8b141313e090df7f131150
Anton Skorup <anton@skorup.se>
2023-01-15 11:49:20 +0100

    Refactor of highscore

--- app/src/main/java/se/skorup/games/base/GamePanel.java
+++ app/src/main/java/se/skorup/games/base/GamePanel.java
@@ -1,14 +1,18 @@
 package se.skorup.games.base;
 
+import se.skorup.API.util.DebugMethods;
 import se.skorup.API.util.Utils;
+import se.skorup.main.manager.helper.SerializationManager;
 
 import javax.swing.JPanel;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.io.IOException;
 
 public class GamePanel extends JPanel
 {
-    protected void drawStart(Graphics2D g, GameFrame gf) {
+    protected void drawStart(Graphics2D g, GameFrame gf)
+    {
         g.setFont(new Font(Font.DIALOG, Font.BOLD, 20));
         g.setColor(Utils.SELECTED_COLOR);
         var width = g.getFontMetrics().stringWidth("Press 'Enter' to start!");
@@ -16,5 +20,27 @@ public class GamePanel extends JPanel
                 "Press 'Enter' to start!",
                 (gf.width() - 7) / 2 - width / 2, gf.height() / 2 - 25
         );
+    }
+
+    /**
+     * Loads the highscore.
+     *
+     * @param path the path to the file.
+     * @return the current highscore.
+     * */
+    protected Score loadHighscore(String path)
+    {
+        Score hs = null;
+
+        try
+        {
+            hs = (Score) SerializationManager.deserializeObject(path);
+        }
+        catch (IOException | ClassNotFoundException e)
+        {
+            DebugMethods.log(e, DebugMethods.LogType.ERROR);
+        }
+
+        return hs == null ? new Score(0) : hs;
     }
 }
