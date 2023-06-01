6693c69dc095d07f42e451c2063aee9b147dbabd
Anton Skorup <anton@skorup.se>
2023-06-01 22:01:31 +0200

    Implemented Localization system.
    TODO: implement correctly in code so that it is stored in the jar file.
    Gradle magic and stuff.

---
+++ app/src/main/java/se/skorup/util/localization/Localization.java
@@ -0,0 +1,119 @@
+package se.skorup.util.localization;
+
+import se.skorup.util.Log;
+import se.skorup.util.io.MyFileReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * A class used for localization of the program.
+ * */
+public class Localization
+{
+    /** The set of the missed lookups in the localization table. */
+    protected static final Set<String> missing;
+
+    private static Map<String, String> map;
+
+    static {
+        missing = ConcurrentHashMap.newKeySet();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (!missing.isEmpty())
+            {
+                var sb = new StringBuilder("Unmatched localizations: ");
+
+                for (var str : missing)
+                    sb.append(str).append(", ");
+
+                sb.delete(sb.length() - 2, sb.length());
+
+                Log.debug(sb);
+            }
+        }));
+    }
+
+    /** You should never be able to instantiate this class. */
+    private Localization() {}
+
+    /**
+     * Gets the value of a key.
+     *
+     * @param key the key that you want the value for.
+     * @return the keys corresponding value iff it exists,
+     *         else it will just return the key.
+     * */
+    public static String getValue(String key)
+    {
+        if (!map.containsKey(key))
+            missing.add(key);
+
+        return map.getOrDefault(key, key);
+    }
+
+    /**
+     * Parses and sets a language file to the current lang file that will be used.
+     *
+     * @param path the path of the lang file.
+     * @return a Map with the key-value pairs from the language file.
+     * @throws IOException iff
+     * */
+    public static Map<String, String> parseLanguageFile(String path) throws IOException
+    {
+        var file = MyFileReader.readFile(new File(path));
+        var lines = file.split("\n");
+        var map = new HashMap<String, String>();
+
+        for (var l : lines)
+        {
+            if (l.trim().length() == 0)
+                continue;
+
+            var keySb = new StringBuilder();
+            var valueSb = new StringBuilder();
+            var i = 0;
+            for (; i < l.length(); i++)
+            {
+                var c = l.toCharArray()[i];
+                if (c == ':')
+                    break;
+
+                keySb.append(c);
+            }
+
+            var started = false;
+            for (; i < l.length(); i++)
+            {
+                var c = l.toCharArray()[i];
+
+                if (!started && c == '\"')
+                {
+                    started = true;
+                    continue;
+                }
+
+                if (!started)
+                    continue;
+
+                if (c == '\"')
+                    break;
+
+                valueSb.append(c);
+            }
+
+            if (valueSb.toString().trim().length() == 0)
+                continue;
+
+            map.put(keySb.toString().trim(), valueSb.toString());
+        }
+
+        Localization.map = map;
+
+        return map;
+    }
+}
---
+++ app/src/test/java/se/skorup/util/localization/TestLocalization.java
@@ -0,0 +1,91 @@
+package se.skorup.util.localization;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestLocalization
+{
+    public static final Map<String, String> SV_SE = Map.of(
+        "ui.button.save", "Spara",
+        "ui.button.exit", "Avsluta",
+        "ui.button.kaka", "Kaka: ",
+        "ui.button.krokodil", "Krokodil:"
+    );
+
+    public static Stream<Arguments> getLocalizationData()
+    {
+        var SV_se = Map.of(
+            "ui.button.save", "Spara",
+            "ui.button.exit", "Avsluta",
+            "ui.button.kaka", "Kaka: ",
+            "ui.button.krokodil", "Krokodil:"
+        );
+
+        var resourceDirectory = Paths.get("src", "test", "resources");
+        var SV_se_PATH = resourceDirectory.toFile().getAbsolutePath() + "/SV_se.lang";
+        var SV_se_hard_PATH = resourceDirectory.toFile().getAbsolutePath() + "/SV_se_hard.lang";
+
+        return Stream.of(
+            Arguments.of(SV_se, SV_se_PATH),
+            Arguments.of(SV_se, SV_se_hard_PATH)
+        );
+    }
+
+    public static Stream<Arguments> getKeyValueData() throws IOException
+    {
+        var resourceDirectory = Paths.get("src", "test", "resources");
+        var SV_se_PATH = resourceDirectory.toFile().getAbsolutePath() + "/SV_se.lang";
+        Localization.parseLanguageFile(SV_se_PATH);
+
+        return Stream.of(
+            Arguments.of(new ArrayList<>(SV_SE.keySet()), new ArrayList<>(SV_SE.values()), new HashSet<>()),
+            Arguments.of(
+                List.of("kaka.kaka", "krokodil.kaka", "ui.button.save"),
+                List.of("kaka.kaka", "krokodil.kaka", SV_SE.get("ui.button.save")),
+                Set.of("kaka.kaka", "krokodil.kaka")
+            ),
+            Arguments.of(new ArrayList<>(SV_SE.keySet()), new ArrayList<>(SV_SE.values()), new HashSet<>()) // Twice just to check that the test code is working with clear.
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getLocalizationData")
+    public void testParseLocalization(Map<String, String> values, String path) throws IOException
+    {
+        var res = Localization.parseLanguageFile(path);
+        assertEquals(values, res, "The parsed values don't match the expected.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("getKeyValueData")
+    public void testGetValue(List<String> keys, List<String> expectedValues, Set<String> expectedMissing)
+    {
+        Localization.missing.clear();
+
+        if (keys.size() != expectedValues.size())
+            fail("Buggy test code!");
+
+        for (var i = 0; i < keys.size(); i++)
+            assertEquals(
+                expectedValues.get(i),
+                Localization.getValue(keys.get(i)),
+                "Key and value pair wrong :("
+            );
+
+        assertEquals(expectedMissing, Localization.missing, "The expected missing elements did not match.");
+    }
+}
