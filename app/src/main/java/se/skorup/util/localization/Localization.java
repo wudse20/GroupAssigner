bfc80cbb38b596ef3c19c7038e092f2f49404ac1
Anton Skorup <anton@skorup.se>
2023-06-01 22:16:33 +0200

    Added build support to build.gralde

--- app/src/main/java/se/skorup/util/localization/Localization.java
+++ app/src/main/java/se/skorup/util/localization/Localization.java
@@ -6,7 +6,6 @@ import se.skorup.util.io.MyFileReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
