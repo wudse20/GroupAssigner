3c24f5fe93ea5347d04fe46b668957f1e78a06ef
Anton Skorup <anton@skorup.se>
2023-06-03 12:07:13 +0200

    Fixed Charset bug

--- app/src/main/java/se/skorup/util/resource/ResourceLoader.java
+++ app/src/main/java/se/skorup/util/resource/ResourceLoader.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 /**
@@ -40,7 +41,7 @@ public class ResourceLoader
 
         if (is != null)
         {
-            var br = new BufferedReader(new InputStreamReader(is));
+            var br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             var lines = new ArrayList<String>();
             var tmp = "";
             while ((tmp = br.readLine()) != null)
